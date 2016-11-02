055ee579abf8ae98abaad6c3c4b9eba3c0fbef7a
AJama1 <AJama1@users.noreply.github.com>
2016-11-02 15:33:53 +0000

    members

---
+++ ESDAssignment/src/model/member.java
@@ -0,0 +1,78 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author a5-jama
+ */
+public class member {
+    public int id;
+    public String name;
+    public String address;
+    public Date dob;
+    public Date dor;
+    public String status;
+    public float balance;
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDor() {
+        return dor;
+    }
+
+    public void setDor(Date dor) {
+        this.dor = dor;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
