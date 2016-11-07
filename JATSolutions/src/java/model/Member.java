d4d2488e5dc0a95cf52d9e6ece82abe510642292
a5-jama <a5-jama@STU-CZC4012N9Y.campus.ads.uwe.ac.uk>
2016-11-07 14:35:50 +0000

    Claims file

---
+++ JATSolutions/src/java/model/Member.java
@@ -0,0 +1,83 @@
+package model;
+
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author a5-jama
+ */
+public class Member {
+    private int id;
+    private String name;
+    private String address;
+    private Date dob;
+    private Date dor;
+    private String status;
+    private float balance;
+  
+    public Member() {
+    }
+    
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDor() {
+        return dor;
+    }
+
+    public void setDor(Date dor) {
+        this.dor = dor;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
---
+++ JATSolutions/src/java/model/User.java
@@ -0,0 +1,48 @@
+package model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author a5-jama
+ */
+public class User {
+    
+    private String id;
+    private String password;
+    private String status;
+
+    public User() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
+}
