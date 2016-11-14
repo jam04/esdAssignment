41eb05a410ce3c170d3520c355812597a5aba27c
jonnyday1 <jonathan3.day@live.uwe.ac.uk>
2016-11-14 14:56:29 +0000

    Creation of submit claim controller

---
+++ JATSolutions/src/java/controller/SubmitClaimController.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Claim;
+import model.DBController;
+
+/**
+ *
+ * @author ja2-day
+ */
+public class SubmitClaimController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String message;
+        DBController jdbc = new DBController((Connection) request.getServletContext().getAttribute("connection"));
+        
+        Claim newClaim = new Claim();
+        newClaim.setMemID(request.getParameter("username"));
+        newClaim.setRationale(request.getParameter("rationale"));
+        newClaim.setAmount(Float.parseFloat((request.getParameter("amount"))));
+        jdbc.submitClaim(request.getParameter("username"), Float.parseFloat(request.getParameter("amount")), request.getParameter("rationale"));
+        message = "Claim submitted";
+        
+        request.setAttribute("message", message);
+        request.getRequestDispatcher("/WEB-INF/docs/submitClaim.jsp").forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
