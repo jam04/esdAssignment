56164a91bb82265f5134cdc6438251daad24b643
r2-augustine <Ruben2.Augustine@live.uwe.ac.uk>
2016-11-14 14:59:01 +0000

    SubmitPaymentController.java
    I have added the submitpaymentcontroller.java in the project

---
+++ JATSolutions/src/java/controller/SubmitPaymentController.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DBController;
+import model.Member;
+import model.Payment;
+
+/**
+ *
+ * @author r2-augustine
+ */
+public class SubmitPaymentController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String message;
+        DBController jdbc = new DBController((Connection) request.getServletContext().getAttribute("connection"));
+
+        if (jdbc.idExist(request.getParameter("id")) == false) {
+            Payment newPayment = new Payment();
+            newPayment.setTypeOfPayment(request.getParameter("typeOfPayment"));
+            newPayment.setAmount(Float.parseFloat((request.getParameter("paymentAmount"))));
+            newPayment.setMemID(request.getParameter("username"));
+            jdbc.makePayment(request.getParameter("username"), Float.parseFloat(request.getParameter("paymentAmount")), request.getParameter("typeOfPayment"));
+            message = "Payment Submitted";
+
+            request.setAttribute("message", message);
+            request.getRequestDispatcher("/WEB-INF/docs/submitPayment.jsp").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
