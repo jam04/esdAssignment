fd7a05048c6668ad6958deb2e6e8718b762f51a4
jonnyday1 <jonathan3.day@live.uwe.ac.uk>
2016-11-23 14:38:40 +0000

    Process Applications
    Membership applications can now be processed.

---
+++ JATSolutions/src/java/controller/ProcessApplicationController.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DBController;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class ProcessApplicationController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String message;
+        DBController jdbc = new DBController((Connection) request.getServletContext().getAttribute("connection"));
+        
+        boolean applicationProcessed = jdbc.processApplication( Integer.parseInt(request.getParameter("paymentId")));
+        
+        if(applicationProcessed){
+            message = "Payment " + request.getParameter("paymentId") + " has been processed";
+        }else{
+            message = "Error processing Payment " + request.getParameter("paymentId") + ", has this claim already been processed?";
+        }
+        request.setAttribute("message", message);
+        request.getRequestDispatcher("/docs/listApplications").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
